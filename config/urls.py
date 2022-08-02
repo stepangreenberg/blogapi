8d445859b65330139e4b46a3b0a6f89b419da68c
Stepan Greenberg <stepan.greenberg@ya.ru>
2022-08-02 15:13:29 +0300

    refactored

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-schema_view = get_schema_view(  # new
+schema_view = get_schema_view(
     openapi.Info(
         title="Blog API",
         default_version="v1",
@@ -23,8 +23,8 @@ urlpatterns = [
     path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api/v1/dj-rest-auth/registration/',
          include('dj_rest_auth.registration.urls')),
-    path('swagger/', schema_view.with_ui(  # new
+    path('swagger/', schema_view.with_ui(
         'swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui(  # new
+    path('redoc/', schema_view.with_ui(
         'redoc', cache_timeout=0), name='schema-redoc'),
 ]
